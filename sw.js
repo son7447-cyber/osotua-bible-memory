cec176b8051ddd7d9f44ca7dfaaf51fe534a919b
son7447-cyber <son7447@gmail.com>
2026-08-02 14:55:18 +0300

    Update service worker cache and asset versions
    Updated cache name and versioning for assets.

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME="osotua-v5-4-2";
+const CACHE_NAME="osotua-v5-5-0";
 const APP_SHELL=[
   "./",
   "./index.html",
-  "./css/style.css?v=5.4.2",
-  "./js/config.js?v=5.4.2",
-  "./js/offline.js?v=5.4.2",
-  "./js/app.js?v=5.4.2",
+  "./css/style.css?v=5.5.0",
+  "./js/config.js?v=5.5.0",
+  "./js/offline.js?v=5.5.0",
+  "./js/app.js?v=5.5.0",
   "./version.json",
   "./manifest.webmanifest",
   "./offline.html",
