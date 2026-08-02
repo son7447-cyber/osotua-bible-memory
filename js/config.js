3dee1d1df76590985a74cf329b8353eb994eb7fc
son7447-cyber <son7447@gmail.com>
2026-08-02 14:47:25 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const OSOTUA_CONFIG={
   supabaseKey:"sb_publishable_N7d4t8ZAZ5Z6KUTsLB5l9w_Blj85XSJ",
   bucket:"recordings",
   referenceBucket:"reference_audio",
-  totalDays:55,
+  totalDays:50,
   totalVerses:50,
   projectStartDate:"2026-07-30",
   adminPin:"0808",
